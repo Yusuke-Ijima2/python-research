0ee1e79f9fde8febd8011f927021a2c7e2130e8a
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-10-20 16:46:36 +0900

    スクレイピングしたものを配列に格納

--- src/sample.py
+++ src/sample.py
@@ -5,5 +5,9 @@ load_url = 'https://zuma-lab.com/'
 data = requests.get(load_url)
 html = BeautifulSoup(data.content, 'html.parser')
 
+html_list = []
+
 for element in html.find_all('h4'):
-  print(element.text)
+  html_list.append(element.text)
+
+print(html_list)
