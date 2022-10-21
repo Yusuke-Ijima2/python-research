22bf7f922cf97d6ee1b8ba6f0881d826324ac88e
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-10-21 14:10:40 +0900

    データ配列を10個に変換

--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 
 load_url = 'https://zuma-lab.com/'
 data = requests.get(load_url)
 html = BeautifulSoup(data.content, 'html.parser')
 
+html_element = html.find_all('h4') #スクレイピングしたデータの配列
 html_list = [] #スクレイピングしたデータをこの変数に格納
 
-for element in html.find_all('h4'):
-  html_list.append(element.text)
+for i in range(10):
+  html_list.append(html_element[i].text)
 
-print(html_list)
+# print(html_list)
 
 app = FastAPI()
 
 @app.get("/scraping-data")
-def read_root():
+def read_root(response: Response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
     return { "data" : html_list }
 
